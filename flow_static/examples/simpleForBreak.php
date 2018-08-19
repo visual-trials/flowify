d2de1c3707a465dc731d4c923787cfe06900649f
Jeffrey <jeffrey@visual-trials.io>
2018-08-19 13:20:30 +0200

    added forBreak example

--- flow_static/examples/simpleForBreak.php
+++ flow_static/examples/simpleForBreak.php
@@ -5,10 +5,10 @@ $myVar = 34;
 $b = 76;
 
 for ($i = 0; $i < 10; $i = $i + 1) {
-    $b = $b - 5;
     if ($b < 20) {
-        continue;
+        break;
     }
+    $b = $b - 5;
 }
 
 $c = $myVar + $b;
