b9392781887db021d99965ee9902f4a406cb2e05
Jeffrey <jeffrey@visual-trials.io>
2018-10-02 22:13:05 +0200

    added comment with code to open a dialog to open a file

--- src/flowify_win32.cpp
+++ src/flowify_win32.cpp
@@ -158,6 +158,30 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmd_line, 
 {
     OutputDebugStringA("Starting!\n");
 
+    /*
+    char filename[ MAX_PATH ];
+
+    OPENFILENAME ofn;
+    ZeroMemory( &filename, sizeof( filename ) );
+    ZeroMemory( &ofn,      sizeof( ofn ) );
+    ofn.lStructSize  = sizeof( ofn );
+    ofn.hwndOwner    = NULL;  // If you have a window to center over, put its HANDLE here
+    ofn.lpstrFilter  = "Text Files\0*.txt\0Any File\0*.*\0";
+    ofn.lpstrFile    = filename;
+    ofn.nMaxFile     = MAX_PATH;
+    ofn.lpstrTitle   = "Select a File, yo!";
+    ofn.Flags        = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
+
+    if (GetOpenFileNameA( &ofn ))
+    {
+    // std::cout << "You chose the file \"" << filename << "\"\n";
+    }
+    else
+    {
+      // ERROR
+    }
+    */    
+    
     init_world();
     
     WNDCLASSA window_class = {};
