0b4e326f9727b17ffa142794520a83796befd982
Jeffrey <jeffrey@visual-trials.io>
2018-10-04 07:08:32 +0200

    reduced to plain c

--- bin/test_build_js.sh
+++ bin/test_build_js.sh
@@ -1,3 +1,3 @@
 source /home/emscripten/emsdk/emsdk_env.sh
 
-emcc src/test.cpp -Os -s WASM=1 -s SIDE_MODULE=1 -o web/wasm/test.wasm
+emcc src/test.c -Os -s WASM=1 -s SIDE_MODULE=1 -o web/wasm/test.wasm
