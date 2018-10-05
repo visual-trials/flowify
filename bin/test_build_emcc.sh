a2cd582ca77c0f65435fdc0a2922119d171db60a
Jeffrey <jeffrey@visual-trials.io>
2018-10-05 22:54:12 +0200

    finally found a way to compile with a proper stack (and without stdlib)

--- bin/test_build_emcc.sh
+++ bin/test_build_emcc.sh
@@ -1,4 +1,6 @@
 source /home/emscripten/emsdk/emsdk_env.sh
 
-emcc src/test.c -Os -s WASM=1 -s SIDE_MODULE=1 -o web/wasm/test_emcc.wasm
-wasm2wat web/wasm/test_emcc.wasm
+# NOTE: this will not work standalone (due to stack issues!) emcc src/test.c -Os -s WASM=1 -s SIDE_MODULE=1 -o web/wasm/test_emcc.wasm
+# So we have to use this commmand, which requires a html or js file (which we don't need atm)
+emcc src/test.c -Os -s WASM=1 -o web/wasm/test_emcc.html -s "EXPORTED_FUNCTIONS=['_init_world', '_update_frame', '_render_frame']"
+# wasm2wat web/wasm/test_emcc.wasm
