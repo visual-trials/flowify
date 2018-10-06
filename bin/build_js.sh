a96caeb8240ba0d1ce2369c1298ee6a2f5bf1c64
Jeffrey <jeffrey@visual-trials.io>
2018-10-06 06:59:54 +0200

    moved all tests to old/test. Added js-stubs for external functions

--- bin/build_js.sh
+++ bin/build_js.sh
@@ -3,7 +3,7 @@ set -e  # stop script on first error
 source /home/emscripten/emsdk/emsdk_env.sh
 
 # NOTE: this will not work standalone (due to stack issues!) emcc src/flowify.cpp -Os -s WASM=1 -s SIDE_MODULE=1 -o web/wasm/flowify.wasm
-emcc src/flowify.cpp -Os -s WASM=1 -o dist/flowify.html -s "EXPORTED_FUNCTIONS=['_init_world', '_update_frame', '_render_frame', '_set_left_mouse_button_data', '_set_right_mouse_button_data', '_set_mouse_wheel_data', '_set_mouse_position_data']"
+emcc src/flowify.cpp -Os -s WASM=1 -o dist/flowify.html --js-library web/js/emcc/external.js -s "EXPORTED_FUNCTIONS=['_init_world', '_update_frame', '_render_frame', '_set_left_mouse_button_data', '_set_right_mouse_button_data', '_set_mouse_wheel_data', '_set_mouse_position_data']"
 cp dist/flowify.wasm web/wasm/flowify.wasm
 # wasm2wat web/wasm/flowify.wasm
 
