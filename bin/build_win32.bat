b492dbd84badf7989f475e81017c7ca79e465c26
Jeffrey <jeffrey@visual-trials.io>
2018-10-13 06:03:17 +0200

    renamed project back to flowify

--- bin/build_win32.bat
+++ bin/build_win32.bat
@@ -1,2 +1,2 @@
-SET PROJECT_NAME=testing_ground
+SET PROJECT_NAME=flowify
 cl /EHsc /Od /Zi /Fddist\vc.pdb src\main_win32.cpp User32.lib Gdi32.lib Winmm.lib Msimg32.lib Comdlg32.lib /DINCLUDE_PROJECT_FILE="\"%PROJECT_NAME%.cpp\"" /DBUILD_FOR_NATIVE_PLATFORM=1 /Fodist\%PROJECT_NAME%.obj /Fedist\%PROJECT_NAME%.exe
