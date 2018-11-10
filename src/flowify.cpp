ac8d1fe3d25ae26cdeefb6fa508ee282afd57690
Jeffrey <jeffrey@visual-trials.io>
2018-11-10 15:00:29 +0100

    buttons now act as menu button: showing different pages

--- src/flowify.cpp
+++ src/flowify.cpp
@@ -32,52 +32,5 @@ extern "C" {
     
     void render_frame()
     {
-        Color4 thin_line_color;
-        Color4 line_color;
-        Color4 fill_color;
-        
-        thin_line_color.r = 200;
-        thin_line_color.g = 200;
-        thin_line_color.b = 200;
-        thin_line_color.a = 255;
-        
-        line_color.r = 10;
-        line_color.g = 40;
-        line_color.b = 155;
-        line_color.a = 100;
-        
-        fill_color.r = 40;
-        fill_color.g = 173;
-        fill_color.b = 255;
-        fill_color.a = 100;
-
-        {
-            draw_line(150, 100, 250, 100, thin_line_color, 1);
-            draw_line(150, 300, 250, 300, thin_line_color, 1);
-            draw_line(150, 500, 250, 500, thin_line_color, 1);
-            
-            draw_line(200, 100, 200, 300, line_color, 8);
-            draw_line(200, 300, 200, 500, line_color, 8);
-        }
-
-        {        
-            draw_line(450, 100, 550, 100, thin_line_color, 1);
-            draw_line(350, 300, 450, 300, thin_line_color, 1);
-            draw_line(450, 500, 550, 500, thin_line_color, 1);
-            
-            draw_line(500, 100, 400, 300, line_color, 8);
-            draw_line(400, 300, 500, 500, line_color, 8);
-        }
-        
-        {
-            draw_line(650, 100, 750, 100, thin_line_color, 1);
-            draw_line(850, 300, 950, 300, thin_line_color, 1);
-            draw_line(1050, 500, 1150, 500, thin_line_color, 1);
-            
-            draw_arced_corner(700, 100, 200, 0, false, line_color, fill_color, 8);
-            draw_arced_corner(1100, 500, 200, 180, true, line_color, fill_color, 8);
-            draw_arced_corner(1300, 300, 200, 90, true, line_color, fill_color, 8);
-        }
-
     }
 }
--- src/testing_renderer.cpp
+++ src/testing_renderer.cpp
@@ -42,7 +42,7 @@ extern "C" {
     {
     }
     
-    void render_frame()
+    void draw_basic_figures()
     {
         Color4 line_color;
         Color4 fill_color;
@@ -82,10 +82,62 @@ extern "C" {
         fill_color.a = 150;
         
         draw_rounded_rectangle(500, 200, 200, 350, 20, line_color, fill_color, 4);
-
-        // Menu
+    }
+    
+    void draw_arced_corners()
+    {
+        Color4 thin_line_color;
+        Color4 line_color;
+        Color4 fill_color;
         
-        WorldData * world = &global_world;
+        thin_line_color.r = 200;
+        thin_line_color.g = 200;
+        thin_line_color.b = 200;
+        thin_line_color.a = 255;
+        
+        line_color.r = 10;
+        line_color.g = 40;
+        line_color.b = 155;
+        line_color.a = 100;
+        
+        fill_color.r = 40;
+        fill_color.g = 173;
+        fill_color.b = 255;
+        fill_color.a = 100;
+
+        {
+            draw_line(150, 100, 250, 100, thin_line_color, 1);
+            draw_line(150, 300, 250, 300, thin_line_color, 1);
+            draw_line(150, 500, 250, 500, thin_line_color, 1);
+            
+            draw_line(200, 100, 200, 300, line_color, 8);
+            draw_line(200, 300, 200, 500, line_color, 8);
+        }
+
+        {        
+            draw_line(450, 100, 550, 100, thin_line_color, 1);
+            draw_line(350, 300, 450, 300, thin_line_color, 1);
+            draw_line(450, 500, 550, 500, thin_line_color, 1);
+            
+            draw_line(500, 100, 400, 300, line_color, 8);
+            draw_line(400, 300, 500, 500, line_color, 8);
+        }
+        
+        {
+            draw_line(650, 100, 750, 100, thin_line_color, 1);
+            draw_line(850, 300, 950, 300, thin_line_color, 1);
+            draw_line(1050, 500, 1150, 500, thin_line_color, 1);
+            
+            draw_arced_corner(700, 100, 200, 0, false, line_color, fill_color, 8);
+            draw_arced_corner(1100, 500, 200, 180, true, line_color, fill_color, 8);
+            draw_arced_corner(1300, 300, 200, 90, true, line_color, fill_color, 8);
+        }
+        
+    }
+    
+    void draw_and_update_button_menu(WorldData * world)
+    {
+        // Draw (and update) button menu
         
         i32 width_button = 50;
         i32 height_button = 50;
@@ -106,6 +158,22 @@ extern "C" {
             }
         }
         
+    }
+    
+    void render_frame()
+    {
+        WorldData * world = &global_world;
+        
+        if (world->active_page_index == 0)
+        {
+            draw_basic_figures();
+        }
+        else if (world->active_page_index == 1)
+        {
+            draw_arced_corners();
+        }
+
+        draw_and_update_button_menu(world);
         
     }
     
