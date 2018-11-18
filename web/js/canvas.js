e944102abcc09967689b0a7171cdda1933be818a
Jeffrey <jeffrey@visual-trials.io>
2018-11-18 20:18:13 +0100

    using devicePixelRatio for now

--- web/js/canvas.js
+++ web/js/canvas.js
@@ -76,7 +76,7 @@ Flowify.canvas = function () {
                 // TODO: if we enable this, we should also respond to it by
                 //       using bigger fonts and bigger shapes. For now this
                 //       is turned off.
-                // my.scale = window.devicePixelRatio
+                my.scale = window.devicePixelRatio
             }        
             my.canvasElement.style.width = window.innerWidth
             my.canvasElement.style.height = window.innerHeight
