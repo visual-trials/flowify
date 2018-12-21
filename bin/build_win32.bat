5a02a15fb22742ce7a1b4686c1b32b17d7e8b7c9
Jeffrey <jeffrey@visual-trials.io>
2018-12-21 12:39:47 +0100

    trying a dpi_aware.manifest

--- bin/build_win32.bat
+++ bin/build_win32.bat
@@ -7,3 +7,5 @@ REM TODO: add more warnings!
 REM TODO: we turned off: C4576 "a parenthesized type followed by an initializer list is a non-standard explicit type conversion syntax"
 REM       how should we initialize struct constants?
 cl /EHsc /O2 /Zi -wd4576 /Fddist\vc.pdb src\win32\main.cpp User32.lib d2d1.lib Dwrite.lib Msimg32.lib Comdlg32.lib /DINCLUDE_PROJECT_FILE="\"../%PROJECT_NAME%.cpp\"" /DBUILD_FOR_NATIVE_PLATFORM=1 /Fodist\%PROJECT_NAME%.obj /Fedist\%PROJECT_NAME%.exe
+
+REM mt.exe -manifest bin\dpi_aware.manifest -outputresource:dist\%PROJECT_NAME%.exe;1
