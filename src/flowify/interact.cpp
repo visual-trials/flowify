2d9a96b92cefc1cafb706ea0845ae591f88977a9
Jeffrey <jeffrey@visual-trials.io>
2019-01-15 13:48:05 +0100

    Unsetting the selected element if the screen is pressed, but not the root element

--- src/flowify/interact.cpp
+++ src/flowify/interact.cpp
@@ -20,23 +20,34 @@
 // TODO: we should check whether these mouse or touch interactions are within the Window of the flowified elements
 void process_interactions(Flowifier * flowifier, Input * input, FlowElement * flow_element)
 {
+    HoveredOrPressed element_hovered_or_pressed = check_hovered_or_pressed(flow_element->absolute_position, flow_element->size, input);
+    
     if (flow_element->type == FlowElement_Root)
     {
         // Resetting hovered_element_index each time we start to process interactions
         flowifier->interaction.hovered_element_index = 0;
+
+        // Unsetting the selected element if the screen is pressed, but not the root element
+        // TODO: instead of the screen rectangle, we should probably use the Window-rectangle here
+        Size2d screen_size = {input->screen.width, input->screen.height};
+        Pos2d screen_position = {};
+        HoveredOrPressed screen_hovered_or_pressed = check_hovered_or_pressed(screen_position, screen_size, input);
+        
+        if (screen_hovered_or_pressed.is_pressed && !element_hovered_or_pressed.is_pressed)
+        {
+            flowifier->interaction.selected_element_index = 0;
+        }
     }
     
-    HoveredOrPressed hovered_or_pressed = check_hovered_or_pressed(flow_element->absolute_position, flow_element->size, input);
-    
-    if (hovered_or_pressed.is_hovered || hovered_or_pressed.is_pressed)
+    if (element_hovered_or_pressed.is_hovered || element_hovered_or_pressed.is_pressed)
     {
         // If the element is hovered, we remember it as the hovered element
-        if (hovered_or_pressed.is_hovered)
+        if (element_hovered_or_pressed.is_hovered)
         {
             flowifier->interaction.hovered_element_index = flow_element->index;
         }
         // If the element is hovered, we remember it as the selected element
-        if (hovered_or_pressed.is_pressed)
+        if (element_hovered_or_pressed.is_pressed)
         {
             flowifier->interaction.selected_element_index = flow_element->index;
         }
